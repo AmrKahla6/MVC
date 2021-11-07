9d188a2d516c53c937098dc28874790ad3b2f3a6
AMR <AMR@extramileegypt.local>
2021-11-07 14:57:17 +0200

    Index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-Amr Here
+<?php
+session_start();
+
+// Sperator /
+define("DS", DIRECTORY_SEPARATOR);
+
+//Root of location
+define('ROOT', dirname(__fILE__));
+
+// root to array
+$url = isset($_SERVER['PATH_INFO']) ? explode('/',ltrim($_SERVER['PATH_INFO'], '/')) : [];
+
+require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
